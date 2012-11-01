d46565034372140c26596ceb7d582b618987a5dd
Matt Richards <matt@lucidmoon.co.uk>
2012-11-01 21:40:09 +0000

    Add playlists collection

---
+++ app/scripts/collections/playlists-collection.js
@@ -0,0 +1,12 @@
+define([
+	'lodash',
+  	'backbone',
+  	'models/playlist-model'
+], function(_, Backbone, PlaylistModel){
+  var collection = Backbone.Model.extend({
+  	model: PlaylistModel
+  });
+  // Return the model for the module
+  return model;
+});
+
