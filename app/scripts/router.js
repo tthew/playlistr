aafe6adff6bc6b4a9a946955a20d9339cd80d6c9
Matt Richards <matt@lucidmoon.co.uk>
2012-11-07 11:33:10 +0000

    Begin implementation of bookmarlet support

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,22 +1,31 @@
 define([
   // Application.
   "app",
-  // Playlists View
-  "views/playlists-view",
-  // "views/playlists-view",  
+    // Event Aggregator
+  "vent",
+  // Views
+  "views/playlists-view"
 ],
 
-function(app, PlaylistsView, PlaylistView) {
+function(app, vent, PlaylistsView, PlaylistView) {
 
   // Defining the application router, you can attach sub routers here.
   var Router = Backbone.Router.extend({
     routes: {
-      "": "index"
+      "": "index",
+      "add_sound/*uri": "add_sound"
     },
 
     index: function() {
+      console.log(this);
+    },
 
+    add_sound: function(uri) {
+      vent.trigger("")
+      console.log(uri);
 
+      $("#plstr-bookmarklet-helper-modal input[name=uri]").val(uri);
+      $("#plstr-bookmarklet-helper-modal").modal("show");
     }
   });
 
